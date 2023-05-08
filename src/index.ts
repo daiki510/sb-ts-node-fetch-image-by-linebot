d1d2a0caa53d6358e92338de2235b60c13619f4a
daiki510 <ddgg7755@gmail.com>
2023-05-08 23:35:37 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,26 @@ import { Client } from '@line/bot-sdk';
 import { load } from 'ts-dotenv';
 import * as fs from 'fs';
 import axios from 'axios';
+import { WebhookEvent, middleware } from '@line/bot-sdk';
+import express, { Application, Request, Response } from 'express';
 
 const env = load({
   CHANNEL_ACCESS_TOKEN: String,
   CHANNEL_SECRET: String,
+  BASE_URL: String,
+  PORT: Number,
+  USER_ID_LIST: String,
 });
+const BASE_URL = env.BASE_URL || 'http://localhost';
+const PORT = env.PORT || 3000;
+const USER_ID_LIST = env.USER_ID_LIST?.split(',') || [];
 
-interface LineMessagingClientConfig {
-  channelAccessToken: string;
-  channelSecret: string;
-}
+const lineMessagingClientConfig = {
+  channelAccessToken: env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: env.CHANNEL_SECRET,
+};
 
-export default class LineMessagingClient {
+class LineMessagingClient {
   private client: Client;
 
   constructor() {
@@ -59,3 +67,33 @@ export default class LineMessagingClient {
     }
   }
 }
+
+const lineMessagingClient = new LineMessagingClient();
+
+const app: Application = express();
+
+app.get('/webhook', async (_: Request, res: Response): Promise<Response> => {
+  return res.status(200).send('Health OK');
+});
+
+app.post(
+  '/webhook',
+  middleware(lineMessagingClientConfig),
+  async (req: Request, res: Response): Promise<Response> => {
+    const events: WebhookEvent[] = req.body.events;
+    await Promise.all(
+      events.map(async (event: WebhookEvent) => {
+        const userId = event.source.userId;
+        if (!userId || !USER_ID_LIST.includes(userId))
+          throw new Error('不正なユーザーのためブロック');
+        if (event.type !== 'message' || event.message.type !== 'image') return;
+        await lineMessagingClient.fetchImageByStream(event.message.id);
+      })
+    );
+    return res.status(200);
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`${BASE_URL}:${PORT}/`);
+});
